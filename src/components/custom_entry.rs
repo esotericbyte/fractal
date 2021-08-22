97560a3efde5f11b03f10e1654dc6f059b158271
Kai A. Hiller <V02460@gmail.com>
2021-08-22 19:49:44 +0200

    components: Add CustomEntry

---
+++ src/components/custom_entry.rs
@@ -0,0 +1,46 @@
+use adw::subclass::prelude::*;
+use gtk::glib;
+use gtk::subclass::prelude::*;
+
+mod imp {
+    use super::*;
+
+    #[derive(Debug, Default)]
+    pub struct CustomEntry {}
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for CustomEntry {
+        const NAME: &'static str = "CustomEntry";
+        type Type = super::CustomEntry;
+        type ParentType = adw::Bin;
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.set_css_name("entry");
+        }
+    }
+
+    impl ObjectImpl for CustomEntry {}
+    impl WidgetImpl for CustomEntry {}
+    impl BinImpl for CustomEntry {}
+}
+
+glib::wrapper! {
+    /// Wrapper object acting as an entry.
+    ///
+    /// Wrap your custom widgets with CustomEntry to get stock entry styling and
+    /// behavior for free.
+    pub struct CustomEntry(ObjectSubclass<imp::CustomEntry>)
+        @extends gtk::Widget, adw::Bin;
+}
+
+impl CustomEntry {
+    pub fn new() -> Self {
+        glib::Object::new(&[]).expect("Failed to create CustomEntry")
+    }
+}
+
+impl Default for CustomEntry {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,6 @@
 mod avatar;
 mod context_menu_bin;
+mod custom_entry;
 mod in_app_notification;
 mod label_with_widgets;
 mod pill;
@@ -8,6 +9,7 @@ mod spinner_button;
 
 pub use self::avatar::Avatar;
 pub use self::context_menu_bin::{ContextMenuBin, ContextMenuBinExt, ContextMenuBinImpl};
+pub use self::custom_entry::CustomEntry;
 pub use self::in_app_notification::InAppNotification;
 pub use self::label_with_widgets::LabelWithWidgets;
 pub use self::pill::Pill;
