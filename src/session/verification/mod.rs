99df1188db7577b7e41b1f85da620fa5eb7bac4c
Julian Sparber <julian@sparber.net>
2021-12-17 18:44:28 +0100

    verification: Allow unused constants

--- src/session/verification/mod.rs
+++ src/session/verification/mod.rs
@@ -6,6 +6,7 @@ pub use self::verification_list::VerificationList;
 
 use std::time::Duration;
 /// The time a verification is valid after it's creation.
+#[allow(dead_code)]
 pub const VERIFICATION_CREATION_TIMEOUT: Duration = Duration::from_secs(60 * 10);
 /// The time a verification is valid after it was received by the client.
 #[allow(dead_code)]
